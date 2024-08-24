813f27f4d2e93da4e953a622af85fa69fa5c8891
SleepertySleep <greezle.gg@mail.ru>
2024-08-24 19:42:45 +0300

    feat: ui added

--- app/lib/pages/login_page.dart
+++ app/lib/pages/login_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -14,8 +13,77 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Login'),
+      ),
+      body: SafeArea(
+          child: _build()
+      ),
     );
   }
-  
+
+  Widget _build(){
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _title(),
+            _login(),
+          ],
+        ),
+    );
+  }
+
+  Widget _title() {
+    return Text(
+        'Recipes',
+        style: TextStyle(
+          fontSize: 35,
+          fontWeight: FontWeight.w300,
+        )
+    );
+  }
+
+  Widget _login() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width * 0.9,
+      height: MediaQuery.sizeOf(context).height * 0.3,
+      child: Form(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Username"
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                  hintText: "Password"
+              ),
+            ),
+            _button(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _button() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width * 0.6,
+      child: ElevatedButton(
+          onPressed: () {},
+          child: Text(
+            'Login',
+          )
+      ),
+    );
+  }
 }
